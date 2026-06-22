4d2a6e5ffe1bbb6c2a80d6670673913c99f00326
Sumit Kumar <ncr2099@gmail.com>
2026-06-22 18:47:30 +0530

    Enable CORS for deployed frontend

--- api.py
+++ api.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://polynomial-builder-v1.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
